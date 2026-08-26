5aa6582c218009710d5ea05fa71bfd09c946ede9
Hungry Detective <hungry-detective@users.noreply.github.com>
2026-08-27 04:56:15 +0600

    Bump version to 1.0.9

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 """Central version information for Shobdo Calok."""
 
 APP_NAME = "ShobdoCalok"
-APP_VERSION = "1.0.8"
+APP_VERSION = "1.0.9"
 
 # GitHub repository used for updates (owner/repo).
 # Change these when the public repo is created.
